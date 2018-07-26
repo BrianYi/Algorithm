82932a778bec99583e02d7c3237526c42aa5c1e2
BrianYi <yijiazhen123@gmail.com>
2018-07-26 11:54:33 +0800

    made changes

--- Algorithm/Tree.h
+++ Algorithm/Tree.h
@@ -5,6 +5,16 @@ ALG_NAMESPACE_BEG
 #define MAX_TREE_SIZE   100
 typedef int ElemType;
 
+typedef struct BiTNode
+{
+    ElemType        data;
+    struct BiTNode  *lchild, *rchild;
+}BiTNode, *BiTree;
 
+Status CreateBiTree(BiTree &T);
+Status PreOrderTraverse(BiTree T, Status(*Visit)(ElemType e));
+Status InOrderTraverse(BiTree T, Status(*Visit)(ElemType e));
+Status PostOrderTraverse(BiTree T, Status(*Visit)(ElemType e));
+Status LevelOrderTraverse(BiTree T, Status(*Visit)(ElemType e));
 
 ALG_NAMESPACE_END
